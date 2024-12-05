ede101d205a9320018796b63f63815fdd76a31b5
Pham Phat <phamphat2003vn@gmail.com>
2024-12-05 13:56:29 +0700

    handle save to PDF

--- src/components/Admin/ManageBooking/ModelDetail.jsx
+++ src/components/Admin/ManageBooking/ModelDetail.jsx
@@ -1,12 +1,76 @@
-import { Modal } from "antd";
+import { Modal, message } from "antd";
 import React from "react";
+import { LogoMain } from "../../../Icon/IconStorage";
+import jsPDF from "jspdf";
 
 const ModelDetail = ({ record, isModalVisible, handleModalClose }) => {
+    const myFont = new FontFace("Times", "url(/public/times.ttf)"); // Tải font chữ từ file .ttf
+
+    // Hàm để tạo PDF từ nội dung trong Modal
+    const handleDownloadPDF = () => {
+        if (!record) {
+            message.error("Không có dữ liệu để in");
+            return;
+        }
+
+        // Tạo một đối tượng jsPDF mới
+        const doc = new jsPDF();
+        // add the font to jsPDF
+        doc.addFileToVFS("MyFont.ttf", myFont);
+        doc.addFont("MyFont.ttf", "MyFont", "normal");
+        doc.setFont("MyFont");
+        // Thêm logo vào PDF với kích thước và vị trí
+        const imageWidth = 100; // Chiều rộng của ảnh
+        const imageHeight = 50; // Chiều cao của ảnh
+        doc.addImage(
+            "/public/main-logo.png",
+            "PNG",
+            10,
+            10,
+            imageWidth,
+            imageHeight
+        ); // Đặt ảnh ở vị trí (10,10) với kích thước 100x50
+
+        // Thiết lập font chữ và kích thước
+        // Thử dùng font "times" thay vì "Times"
+        doc.setFontSize(14); // Đặt kích thước font chữ
+
+        // Thêm tiêu đề vào PDF
+        doc.setFontSize(24); // Tiêu đề lớn hơn
+        doc.text("Thông tin hóa đơn", 10, 70); // Đặt tiêu đề ở vị trí (10,70)
+
+        // Thêm thông tin booking vào PDF
+        doc.setFontSize(16); // Kích thước font chữ cho thông tin chi tiết
+        doc.setFont("times", "normal"); // Đặt lại font
+        doc.text(`Booking ID: ${record.id}`, 10, 80);
+        doc.text(`User ID: ${record.userId}`, 10, 90);
+        doc.text(`Phòng: ${record.room?.name}`, 10, 100);
+        doc.text(`Mô tả: ${record.room?.description}`, 10, 110);
+        doc.text(`Tổng tiền: ${record?.totalPrice} VNĐ`, 10, 120);
+        doc.text(`Ngày nhận phòng: ${record.formattedDateIn}`, 10, 130);
+        doc.text(`Ngày trả phòng: ${record.formattedDateOut}`, 10, 140);
+        doc.text(`Người đặt phòng: ${record.user?.name}`, 10, 150);
+        doc.text(`Thông tin liên hệ: ${record.user?.phone}`, 10, 160);
+        doc.text(`Email: ${record.user?.email}`, 10, 170);
+        doc.text(
+            `Trạng thái thanh toán: ${
+                record.paymentStatus ? "Đã thanh toán" : "Chưa thanh toán"
+            }`,
+            10,
+            180,
+            { maxWidth: 180 }, // Đảm bảo text không bị vượt quá chiều rộng
+            { align: "left" }
+        );
+
+        // Lưu file PDF
+        doc.save(`HoaDon_${record.id}.pdf`);
+    };
+
     return (
         <>
             <Modal
-                title="Chi tiết đặt phòng"
-                visible={isModalVisible}
+                title={<LogoMain />}
+                open={isModalVisible}
                 onCancel={handleModalClose}
                 footer={null}
             >
@@ -14,12 +78,15 @@ const ModelDetail = ({ record, isModalVisible, handleModalClose }) => {
                     <>
                         <div className="py-3 px-4">
                             <div className="flex items-center">
-                                <h3 className="text-main px-3 w-9/12 mb-4 font-semibold text-2xl  text-center ">
+                                <h3 className="text-main px-3 w-9/12 mb-4 font-semibold text-2xl text-center">
                                     Thông tin hóa đơn
                                 </h3>
-                                <div className=" w-3/12">
+                                <div className="w-3/12">
                                     <button
-                                        onClick={handleModalClose}
+                                        onClick={() => {
+                                            handleDownloadPDF(); // Gọi hàm tạo và tải PDF
+                                            handleModalClose(); // Đóng Modal
+                                        }}
                                         className="bg-red-500 text-white mb-3 hover:text-main border hover:bg-white hover:border-red-500 border- px-3 py-1 rounded-xl"
                                     >
                                         In hóa đơn
@@ -41,8 +108,10 @@ const ModelDetail = ({ record, isModalVisible, handleModalClose }) => {
                                 </p>
                             </div>
                             <div className="flex justify-between items-center gap-3 mb-3 w-full">
-                                <strong>Mô tả phòng:</strong>{" "}
-                                <p>{record.room?.description}</p>
+                                <strong>Mô tả:</strong>
+                                <p className="truncate">
+                                    {record.room?.description}
+                                </p>
                             </div>
                             <div className="flex justify-between items-center gap-3 mb-3 w-full">
                                 <strong>Tổng tiền:</strong>
@@ -51,27 +120,27 @@ const ModelDetail = ({ record, isModalVisible, handleModalClose }) => {
                                 </p>
                             </div>
                             <div className="flex justify-between items-center gap-3 mb-3 w-full">
-                                <strong>Ngày nhận phòng:</strong>{" "}
-                                <p> {record.formattedDateIn}</p>
+                                <strong>Ngày nhận phòng:</strong>
+                                <p>{record.formattedDateIn}</p>
                             </div>
                             <div className="flex justify-between items-center gap-3 mb-3 w-full">
-                                <strong>Ngày trả phòng:</strong>{" "}
+                                <strong>Ngày trả phòng:</strong>
                                 <p>{record.formattedDateOut}</p>
                             </div>
                             <div className="flex justify-between items-center gap-3 mb-3 w-full">
-                                <strong>Người đặt phòng:</strong>{" "}
+                                <strong>Người đặt phòng:</strong>
                                 <p>{record.user?.name}</p>
                             </div>
                             <div className="flex justify-between items-center gap-3 mb-3 w-full">
-                                <strong>Thông tin liên hệ:</strong>{" "}
+                                <strong>Thông tin liên hệ:</strong>
                                 <p>{record.user?.phone}</p>
                             </div>
                             <div className="flex justify-between items-center gap-3 mb-3 w-full">
-                                <strong>Email:</strong>{" "}
+                                <strong>Email:</strong>
                                 <p>{record.user?.email}</p>
                             </div>
                             <div className="flex justify-between items-center gap-3 mb-3 w-full">
-                                <strong>Trạng thái thanh toán:</strong>{" "}
+                                <strong>Trạng thái thanh toán:</strong>
                                 <p
                                     className={`${
                                         record.paymentStatus
