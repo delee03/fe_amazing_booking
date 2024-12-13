5a022cab163cf777c981f30d092de75384e4eda6
Pham Phat <phamphat2003vn@gmail.com>
2024-12-13 11:28:42 +0700

    feat: changeport

--- src/service/config.js
+++ src/service/config.js
@@ -1,11 +1,10 @@
 import axios from "axios";
-import dotenv from "dotenv";
 import { getTokeStorage } from "../utils/localStorage";
 //intial axios instance
 //intial dotenv config
 
 export const http = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL || "http://localhost:3000",
+    baseURL: import.meta.env.VITE_BASE_URL,
     timeout: 30000,
     headers: {
         Authorization: `Bearer ${getTokeStorage("token") || ""}`,
